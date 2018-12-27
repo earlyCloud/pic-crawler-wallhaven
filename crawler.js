1b8ffab2155a3ad1ce5b7b0bd41308f5cff03563
yun <1272558578@qq.com>
2018-12-27 18:36:45 +0800

    original commit

---
+++ crawler.js
@@ -0,0 +1,106 @@
+const fs = require('fs')
+const Crawler = require('crawler')
+const koa = require('koa')
+const cheerio = require('cheerio')
+
+
+let page = 1, 
+args = process.argv.slice(2),
+dist = './images/',
+queryString = args.length ? args[0] : "",
+emptyCount = 0
+
+queryString && (dist = dist + queryString + '/')
+
+getList()
+
+function getImg(imgOption){
+	const imgCrawler = new Crawler({
+		encoding: null,
+		jQuery: false,
+		maxConnections: 10,
+		headers: {
+			'Host': 'alpha.wallhaven.cc',
+			'Referer': 'https://alpha.wallhaven.cc',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'
+		},
+		callback(err, res, done){
+			if(err){
+				console.log('err', error)
+			}else{
+				if(res.statusCode === 200){
+					if(!fs.existsSync(dist)){
+						fs.mkdirSync(dist)
+					}
+					const $ = res.$
+					fs.createWriteStream(`${ dist }${ res.options.imgid }.jpg`).write(res.body)
+					console.log(`${ res.options.imgid }.jpg complete`)
+				}
+			}
+			done()
+		}
+	})
+	imgCrawler.queue(imgOption)
+}
+
+
+function getDetailPage(imgArr = []){
+	const pageCrawler = new Crawler({
+		jQuery: 'cheerio',
+		maxConnections : 10,
+		callback(err, res, done){
+			if(err){
+				console.log(err)
+			}else{
+				if(res.statusCode === 200){
+					let $ = res.$
+					let imgid = $("#wallpaper").attr('data-wallpaper-id')
+					let imgurl = `https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-${ imgid }.jpg`
+					console.log(imgurl)
+					getImg({
+						uri: imgurl,
+						imgid: imgid
+					})
+				}
+			}
+			done()
+		}
+	})
+	pageCrawler.queue(imgArr)
+}
+
+function getList(){
+	let urlArr = [], 
+	url = `https://alpha.wallhaven.cc/search?q=${ queryString }&page=${ page }`,
+	listCrawler = new Crawler({
+		jQuery: 'cheerio',
+		maxConnections: 10,
+		skipDuplicates: true,
+		callback(err, res, done){
+			if(err){
+				console.log(err)
+			}else{
+				let $ = res.$
+				let links = $('section.thumb-listing-page a.preview')
+				links.each((index, link) => {
+					$(link).attr('href') && urlArr.push($(link).attr('href'))
+				})
+				console.log(`page: ${ page }`, urlArr)
+				if(page < 100){
+					page ++ 
+					getDetailPage(urlArr)
+					if(urlArr.length){
+						setTimeout(getList, 60000)
+					}else if(emptyCount < 3){
+						emptyCount ++ 
+						getList()
+					}
+				}
+			}
+			done()
+		}
+	})
+	listCrawler.queue(url)
+}
+
+
